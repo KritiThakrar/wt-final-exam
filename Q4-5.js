7d44343049ae06ef7d2e7070ed91f6da4e7709e1
Kriti Thakrar <66572570+KritiThakrar@users.noreply.github.com>
2022-12-01 13:24:32 +0530

    Update and rename Q4-5.js to Q4-5.html

--- Q4-5.js
+++
@@ -1,6 +0,0 @@
-function sortArray(myArray)
-{
-    myArray.sort(); 
-    myArray.reverse();
-}
-console.log(sortArray([{productId:1,productName:"Mobile",price:70000},{productId:1,productName:"Mobile",price:70000}]));
